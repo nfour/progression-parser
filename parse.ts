9308efa6e8bbc78d67356bd6a84e54a2fc615740
nfour <novus.nfour@gmail.com>
2017-11-15 21:24:17 +1000

    wew

--- parse.ts
+++ parse.ts
@@ -1,5 +1,39 @@
+import { readFileSync, writeFileSync, mkdirSync } from "fs";
 
-const file = JSON.parse(Buffer.from(require('fs').readFileSync('./prog', 'utf8'), 'base64').toString('utf8'))
+const _dir = console.dir
+console.dir = (v, o = {}) => _dir(v, { depth: 10, colors: true, ...o })
 
+const saveTo = (to, obj) => {
+  writeFileSync(`./out/${to}`, JSON.stringify(obj, null, 2))
+}
 
-console.dir(file, { depth: 10, colors: true })
+interface IBaseFile { preferences_: string }
+interface IProgressionFile { [k: string]: any }
+
+const parseProgressionFile = (blob: string) => {
+  const base: IBaseFile = JSON.parse(
+    Buffer.from(blob, 'base64').toString('utf8')
+  )
+
+  const preferences = JSON.parse(base.preferences_)
+
+  saveTo('preferences.json', preferences)
+
+  const ua = JSON.parse(base['ua.json'])
+
+  saveTo('ua.json', ua)
+
+  const up = JSON.parse(base['up.json'])
+
+  saveTo('up.json', up)
+
+  const fws = JSON.parse(base['fws.json'])
+
+  saveTo('fws.json', fws)
+}
+
+try { mkdirSync('./out') } catch {}
+
+parseProgressionFile(readFileSync('./prog', 'utf8'))
+
+// console.dir(file)
